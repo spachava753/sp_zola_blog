e3d11b180751ba3313487dea2aff1dbc0d2be3ee
Shashank Pachava <spachava753@gmail.com>
2021-08-07 18:00:50 -0400

    Update deployment script

--- deploy.sh
+++ deploy.sh
@@ -9,17 +9,23 @@ pages=${CF_PAGES:-0}
 # If not running in Cloudflare workers, just run `zola serve`
 
 if [[ "${pages}" == 0 ]]; then
-  zola serve;
-  exit 0
+  zola serve
 fi
+
+echo "building site";
+
+# print environment variables
+env
 
 if [[ -z "${CF_PAGES_BRANCH}" ]]; then
 
   if [[ "${CF_PAGES_BRANCH}" == "main" ]]; then
+    echo "building site for production";
     zola build
   else
 
     if [[ -z "${CF_PAGES_COMMIT_SHA}" ]]; then
+      echo "building site for branch ${CF_PAGES_BRANCH}";
       zola build --base-url "https://${CF_PAGES_COMMIT_SHA}.sp-zola-blog.pages.dev"
     else
       echo "env CF_PAGES_COMMIT_SHA not set!";
